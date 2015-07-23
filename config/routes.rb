e424627f7de5a916a516dc216dff08f24979b394
Diogo Freitas <diogolaco@gmail.com>
2015-07-23 18:52:03 -0300

    prototipo primeira exp professor

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Site::Application.routes.draw do
   match '/home' => 'pages#home'
   match '/home2' => 'pages#home2'
   match '/home3' => 'pages#home3'
+  match '/home4' => 'pages#home4'
+  match '/home5' => 'pages#home5'
+  match '/home6' => 'pages#home6'
+  match '/home7' => 'pages#home7'
+  match '/home8' => 'pages#home8'
+  match '/home9' => 'pages#home9'
 
 end
