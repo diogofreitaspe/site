5788b2097d9f93538612d5b9d423e8778be47bfc
Diogo Freitas <diogolaco@gmail.com>
2015-07-23 19:51:01 -0300

    ultimos ajustes do prototipo

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Site::Application.routes.draw do
   match '/home2' => 'pages#home2'
   match '/home3' => 'pages#home3'
   match '/home4' => 'pages#home4'
+  match '/home4a' => 'pages#home4a'
+  match '/home4b' => 'pages#home4b'
   match '/home5' => 'pages#home5'
   match '/home6' => 'pages#home6'
   match '/home7' => 'pages#home7'
